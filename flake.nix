1c9d3b7c04e3cff3f1c625074d3c26a8e39bf2ee
UnveilingPartner <github.bot@acm-sl.org>
2024-02-05 18:48:45 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -33,14 +33,14 @@
         "pythoneda-shared-banner";
       inputs.pythoneda-shared-domain.follows =
         "pythoneda-shared-domain";
-      url = "github:pythoneda-shared-def/application/0.0.52";
+      url = "github:pythoneda-shared-def/application/0.0.53";
     };
     pythoneda-shared-domain = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-banner.follows =
         "pythoneda-shared-banner";
-      url = "github:pythoneda-shared-def/domain/0.0.31";
+      url = "github:pythoneda-shared-def/domain/0.0.32";
     };
   };
   outputs = inputs:
